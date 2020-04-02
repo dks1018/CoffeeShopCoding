92c3bb7551a8cf5fb91f432b2077977098db1213
SYN <56562806+dks1018@users.noreply.github.com>
2020-04-02 00:36:14 -0600

    fixed tv bits programs

--- MicrosoftInterview/Interview 3/tv_bits.py
+++ MicrosoftInterview/Interview 3/tv_bits.py
@@ -8,7 +8,7 @@ counter = 0
 
 # Create randomized array
 while counter < rand_num1:
-    rand_num2 = random.randint(1,20)
+    rand_num2 = random.randint(0,20)
     random_numbers_Arr.append(rand_num2)
     counter += 1
 print(random_numbers_Arr)
@@ -16,18 +16,29 @@ print(random_numbers_Arr)
 # count repeated numbers in the array and store in new array
 counter = 1
 solution_arr = []
-for i in random_numbers_Arr[1:]:
-    if(i == random_numbers_Arr[-1]):
+for i in random_numbers_Arr[1::]:
+    if(i == random_numbers_Arr[i-1]):
         counter += 1
     else:
         solution_arr.append("{Set")
         solution_arr.append(counter)
+        solution_arr.append(":")
         solution_arr.append(i)
         solution_arr.append("}")
         counter = 1
-
+    solution_arr.append("{Set")
+    solution_arr.append(counter)
+    solution_arr.append(":")
+    solution_arr.append(i)
+    solution_arr.append("}")
+# strip out '
 newString = str(solution_arr).replace("'","")
+
+# strip out ,
 newString2 = newString.replace(",","")
 
+# print string representation of array
 print(newString2)
+
+
 #print("\n",str(solution_arr).strip('[]'))
